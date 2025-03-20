a9c68e6b5e24851d743b447fb29e9ed1cb56b7b5
manbearshark <igorkrtolica@max-slaughter-mbp.local>
2025-03-19 23:01:10 -0700

    working ws locally + local config

--- index.js
+++ index.js
@@ -4,9 +4,14 @@ import {
 } from "@aws-sdk/client-bedrock-runtime";
 
 import express from "express";
+import { WebSocketServer } from "ws";
 
 const app = express();
 const port = 3000;
+const wsServer = new WebSocketServer({ noServer: true });
+const server = app.listen(port, () => {
+  console.log(`Access your application at`, `http://localhost:${port}`)
+});
 
 // Create a Bedrock Runtime client in the AWS Region you want to use and this service is running in.
 let client;
@@ -55,6 +60,44 @@ app.get('/health', (req, res) => {
   res.send('Healthy');
 });
 
-app.listen(port, () => {
-  console.log(`Access your application at`, `http://localhost:${port}`)
+// WebSocket handlers
+
+server.on('upgrade', (request, socket, head) => {
+  wsServer.handleUpgrade(request, socket, head, (socket) => {
+    wsServer.emit('connection', socket, request);
+  })
+});
+
+wsServer.on('connection', (socket, request) => {
+  socket.on('message', async (message) => {
+    try {
+      const userMessage = JSON.parse(message).message;
+      const conversation = [
+        {
+          role: "user",
+          content: [{ text: userMessage }],
+        },
+      ];
+      console.log(`Sending message: ${userMessage}`);
+
+      // Create a command with the model ID, the message, and a basic configuration.
+      const command = new ConverseCommand({
+        modelId: inferenceProfileArn,
+        messages: conversation,
+        inferenceConfig: { maxTokens: 512, temperature: 0.5, topP: 0.9 },
+      });
+      const response = await client.send(command);
+      const answer = response;
+      console.log(`Got answer: ${answer}`);
+
+      socket.send(JSON.stringify({ message: answer }));
+    } catch (error) {
+      console.error(error);
+      socket.send(
+        JSON.stringify({
+          message: "An error occurred while processing your request.",
+        })
+      );
+    }
+  })
 });
