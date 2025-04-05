7a051bd25a3f23862aec256c99a183ccbb961b21
manbearshark <igorkrtolica@max-slaughter-mbp.local>
2025-04-05 10:55:58 -0700

    Twilio setup call works - sends correct params

--- app/index.js
+++ app/index.js
@@ -1,104 +1,81 @@
-import {
-  BedrockRuntimeClient,
-  ConverseCommand,
-} from "@aws-sdk/client-bedrock-runtime";
-
 import express from "express";
+import bodyParser from "body-parser";
 import { WebSocketServer } from "ws";
 import { setupCallPostHandler } from './lib/setup-call-post-handler.mjs';
+import { onConnectWebsocketHandler } from './lib/onconnect-websocket-handler.mjs';
+import querystring from 'node:querystring';
+import { v4 as uuidv4 } from 'uuid';
 
 const app = express();
 const port = 3000;
 const wsServer = new WebSocketServer({ noServer: true });
 const server = app.listen(port, () => {
-  console.log(`Access your application at`, `http://localhost:${port}`)
+  console.log(`App is ready.`);
 });
 
-// Create a Bedrock Runtime client in the AWS Region you want to use and this service is running in.
-let client;
-const inferenceProfileArn = process.env.INFERENCE_PROFILE_ARN;
-const region = process.env.AWS_REGION;
+app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
-try {
-  client = new BedrockRuntimeClient({ region });
-  console.log("Client");
-  console.log(`Inference Profile ARN: ${inferenceProfileArn}`);
-} catch (err) {
-  console.error(err);
-}
-
-app.get('/', async (req, res) => {
+// Twilio will send a POST request to this endpoint when estabilishing a call
+app.post('/call-setup-restaurant-order', async (req, res) => {
   try {
-    const userMessage =
-      "Describe the purpose of a 'hello world' program in one line.";
-    const conversation = [
-      {
-        role: "user",
-        content: [{ text: userMessage }],
-      },
-    ];
-    console.log(`Sending message: ${userMessage}`);
-
-    // Create a command with the model ID, the message, and a basic configuration.
-    const command = new ConverseCommand({
-      modelId: inferenceProfileArn,
-      messages: conversation,
-      inferenceConfig: { maxTokens: 512, temperature: 0.5, topP: 0.9 },
-    });
-    const response = await client.send(command);
-    const answer = response;
-    console.log(`Got answer: ${answer}`);
-
-    res.send(answer);
+    // 1) Parse BODY of request to extract Call Details
+    // let bufferObj = Buffer.from(req.body, "base64");    
+    // let twilio_body = querystring.decode(bufferObj.toString("utf8"));
+    // 2) Generate a UUID for this call session
+    let callSessionId = uuidv4();
+    console.log("Twilio Body: ", JSON.stringify(req.body));
+    const twilio_body = req.body;
+    // 3) Call the setupCallPostHandler function to handle the Twilio request
+    const twilmlResponse = await setupCallPostHandler(twilio_body, callSessionId);
+    // 4) Send the TwiML response back to Twilio
+    res.status(200).type('application/xml').send(twilmlResponse);
   } catch (error) {
     console.error(error);
     res.status(500).send("An error occurred while processing your request.");
   }
 });
 
+// Healthscheck endpoint for load balancers
 app.get('/health', (req, res) => {
   // Health check endpoint
   res.send('Healthy');
 });
 
-// WebSocket handlers
-
+// WebSocket handlers - this server is shared with the HTTP server
 server.on('upgrade', (request, socket, head) => {
   wsServer.handleUpgrade(request, socket, head, (socket) => {
-    wsServer.emit('connection', socket, request);
+    const callSessionId = uuidv4();
+    wsServer.emit('connection', socket, request, head, callSessionId);
   })
 });
 
-wsServer.on('connection', (socket, request) => {
-  socket.on('message', async (message) => {
-    try {
-      const userMessage = JSON.parse(message).message;
-      const conversation = [
-        {
-          role: "user",
-          content: [{ text: userMessage }],
-        },
-      ];
-      console.log(`Sending message: ${userMessage}`);
+wsServer.on('connection', (socket, request, head, callSessionId) => {
+  console.log('Client connected request: ', JSON.stringify(request));
+  console.log('Client connected headers: ', JSON.stringify(head));
 
-      // Create a command with the model ID, the message, and a basic configuration.
-      const command = new ConverseCommand({
-        modelId: inferenceProfileArn,
-        messages: conversation,
-        inferenceConfig: { maxTokens: 512, temperature: 0.5, topP: 0.9 },
-      });
-      const response = await client.send(command);
-      const answer = response;
-      console.log(`Got answer: ${answer}`);
+  socket.on('message', async (message, callSessionId) => {
 
-      socket.send(JSON.stringify({ message: answer }));
-    } catch (error) {
-      console.error(error);
-      socket.send(
-        JSON.stringify({
-          message: "An error occurred while processing your request.",
-        })
-      );
-    }
-  })
+  });
 });
+
+    // try {
+    //   const userMessage = JSON.parse(message).message;
+    //   const conversation = [
+    //     {
+    //       role: "user",
+    //       content: [{ text: userMessage }],
+    //     },
+    //   ];
+    //   console.log(`Sending message: ${userMessage}`);
+
+    //   // Add onconnect call here...
+
+    //   socket.send(JSON.stringify({ message: answer }));
+    // } catch (error) {
+    //   console.error(error);
+    //   socket.send(
+    //     JSON.stringify({
+    //       message: "An error occurred while processing your request.",
+    //     })
+    //   );
+    // }
