a4e5d34cbb3e68f818ad1464fc0d730e418d298e
George Wolf <1186878+manbearshark@users.noreply.github.com>
2025-05-01 15:19:28 -0700

    Fix inference profile cloud settings (#4)
    * Added binary image files for diagrams + slides update
    
    * removed inf profiel and added param for setting cross-region profile
    
    * Updates to the deployment files
    
    ---------
    
    Co-authored-by: manbearshark <igorkrtolica@max-slaughter-mbp.local>

--- build-images.sh
+++ build-images.sh
@@ -1,4 +1,4 @@
-aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin 971461683291.dkr.ecr.us-west-2.amazonaws.com
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 971461683291.dkr.ecr.us-east-1.amazonaws.com
 docker build -t twilio-conversation-relay-bedrock-services --platform linux/amd64 .
-docker tag twilio-conversation-relay-bedrock-services:latest 971461683291.dkr.ecr.us-west-2.amazonaws.com/twilio-conversation-relay-bedrock-services:latest
-docker push 971461683291.dkr.ecr.us-west-2.amazonaws.com/twilio-conversation-relay-bedrock-services:latest
+docker tag twilio-conversation-relay-bedrock-services:latest 971461683291.dkr.ecr.us-east-1.amazonaws.com/twilio-conversation-relay-bedrock-services:latest
+docker push 971461683291.dkr.ecr.us-east-1.amazonaws.com/twilio-conversation-relay-bedrock-services:latest
