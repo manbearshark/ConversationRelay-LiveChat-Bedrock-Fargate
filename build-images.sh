ed4d3c1c166cafbfc2f1dae9307818e94b003a18
manbearshark <igorkrtolica@max-slaughter-mbp.local>
2025-05-04 14:35:21 -0700

    Consolidated template files

--- build-images.sh
+++ build-images.sh
@@ -1,4 +1,7 @@
-aws ecr get-login-password --region us-east-1 --profile ConversationRelayPrgDanBartlett | docker login --username AWS --password-stdin 703671914627.dkr.ecr.us-east-1.amazonaws.com
+ECR_REPO=971461683291.dkr.ecr.us-east-1.amazonaws.com
+ECR_REPO_NAME=twilio-conversation-relay-bedrock-services
+
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin $ECR_REPO
 docker build -t bedrock-cr --platform linux/amd64 .
-docker tag bedrock-cr:latest 703671914627.dkr.ecr.us-east-1.amazonaws.com/bedrock-cr:latest
-docker push 703671914627.dkr.ecr.us-east-1.amazonaws.com/bedrock-cr:latest
+docker tag bedrock-cr:latest $ECR_REPO/$ECR_REPO_NAME:latest
+docker push $ECR_REPO/$ECR_REPO_NAME:latest
