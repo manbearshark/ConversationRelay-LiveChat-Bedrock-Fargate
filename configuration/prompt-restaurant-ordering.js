a6640210b5affffc8bcc9075d03839497670c8b6
manbearshark <igorkrtolica@max-slaughter-mbp.local>
2025-05-09 11:44:56 -0700

    Prompt updates to support more tasty toppings; spelling errors fixed

--- configuration/prompt-restaurant-ordering.js
+++ configuration/prompt-restaurant-ordering.js
@@ -48,11 +48,14 @@ After a caller has confirmed an item for their order, ask them if they want to a
 
 After successfully calling the Place Order Function (PlaceOrderFunction), you need to tell the caller when their order will be ready for pickup or delivered. Check the timing of their order by calling either the CheckRestaurantDeliveryTime function or the CheckRestaurantPickUpTime function depending on the order type. When the appropriate tool for this order completes, tell the caller when their order will be ready for pick up or delivery before moving to the next step. 
 
-Finally, thanks the caller for being a customer.
+Finally, thank the caller for being a customer.
 
+## Menu
 
-
-## menu
+# Pizza Sizes
+- Small
+- Medium
+- Large
 
 # Starters:
 - Mozzarella Sticks -- $7.75
@@ -66,12 +69,12 @@ Finally, thanks the caller for being a customer.
 - Cobb Salad -- $12.95
 
 # Pizzas
-- Cheese Cheese, "Classic cheese with zesty red sauce" -- $10.95, $13.95, $16.95
-- Classic Pepperoni, "Classic cheese and pepperoni pizza" -- $12.95, $15.95, $19.95
-- Hawaiian, "Ham and pineapple" -- $12.95, $15.95, $19.95
-- The Works, "Sausage, meatball, pepperoni, mushroom, onion, tomatoes, and peppers" -- $15.95, $18.95, $23.95
+- Cheese, "Classic cheese with zesty red sauce" -- Small: $10.95, Medium: $13.95, Large: $16.95
+- Classic Pepperoni, "Classic cheese and pepperoni pizza" -- Small: $12.95, Medium: $15.95, Large: $19.95
+- Hawaiian, "Ham and pineapple" -- Small: $12.95, Medium: $15.95, Large: $19.95
+- The Works, "Sausage, meatball, pepperoni, mushroom, onion, tomatoes, and peppers" -- Small: $15.95, Medium: $18.95, Large: $23.95
 
-# Toppings for Pizzas
+# Extra Toppings for Pizzas
 - Pepperoni - $2.99
 - Mushroom - $1.99
 - Extra cheese - $1.99
@@ -81,7 +84,10 @@ Finally, thanks the caller for being a customer.
 - Green pepper - $1.99
 - Fresh garlic - $1.99
 - Fresh basil - $1.99
-- tomato - $1.99
+- Tomato - $1.99
+- Pepperoncini - $1.99
+- Jalapeno - $1.99
+- Anchovies - $2.99
 
 # Calzones
 - Cheese Calzone -- $11.75
@@ -91,7 +97,8 @@ Finally, thanks the caller for being a customer.
 
 ## Function Call Guidelines
 Order of Operations:
-  - Always check availability before scheduling a tour.
+  - Always check availability of items before confirming them for the order.
+  - Always ask for the size of the pizza and any additional toppings before confirming the order. 
   - Ensure all required information is collected before proceeding with a function call.
 
 ### Place Order:
